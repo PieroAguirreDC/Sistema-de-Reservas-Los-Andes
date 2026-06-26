3f8b1b4ef61ef43c480e8f8de634e044f30fb516
PieroAguirreDC <pierogaguirredc@gmail.com>
2026-06-25 23:05:35 -0500

    fix(Piero): CKV_AWS_130 en subredes vpc // desactivar asignacion automatica de ip publica en subredes

--- Sistema-de-Reservas/iac/vpc/main.tf
+++ Sistema-de-Reservas/iac/vpc/main.tf
@@ -68,7 +68,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = local.public_cidrs[count.index]
   availability_zone       = local.azs[count.index]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "${local.name_prefix}-public-${local.azs[count.index]}"
